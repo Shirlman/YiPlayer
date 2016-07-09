f17d372b48952dc1ab497ba855dffc77a1fdedd2
Shirlman <812300445@qq.com>
2016-07-09 17:31:55 +0800

    replace with vlc for android 2.02
    video view not usable

--- app/src/main/java/com/shirlman/yishi/ui/activities/VideoActivity.java
+++ app/src/main/java/com/shirlman/yishi/ui/activities/VideoActivity.java
@@ -13,15 +13,10 @@ import com.shirlman.yishi.ui.widgets.VideoController;
 
 import java.io.File;
 
-import io.vov.vitamio.MediaPlayer;
-import io.vov.vitamio.Vitamio;
-import io.vov.vitamio.widget.VideoView;
-
 /**
  * Created by KB-Server on 2016/6/22.
  */
 public class VideoActivity extends AppCompatActivity {
-    private VideoView mVideoView;
     private long mPosition;
     private TextView mSubtitleView;
 
@@ -33,62 +28,14 @@ public class VideoActivity extends AppCompatActivity {
 
         final VideoInfo videoInfo = (VideoInfo) getIntent().getExtras().getSerializable(VideoInfo.class.getSimpleName());
 
-        Vitamio.isInitialized(getApplicationContext());
-
         setContentView(R.layout.video_view);
-        mVideoView = (VideoView) findViewById(R.id.video_view);
+
         mSubtitleView = (TextView) findViewById(R.id.video_subtitle);
 
-        mVideoView.setVideoPath(videoInfo.getPath());
-        mVideoView.requestFocus();
-
-        final VideoController videoController = new VideoController(this, videoInfo);
-        mVideoView.setMediaController(videoController);
-
-        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                // optional need Vitamio 4.0
-                mediaPlayer.setPlaybackSpeed(1.0f);
-
-                if (mPosition > 0) {
-                    // 若不调用 VideoView.getDuration() 就进行 seekTo() 那么 seekTo() 无效！
-                    mVideoView.getDuration();
-                    mVideoView.seekTo(mPosition);
-                }
-
-                String subtitlePath = videoInfo.getPath().substring(0, videoInfo.getPath().lastIndexOf(".")) + ".srt";
-
-                if(new File(subtitlePath).exists()) {
-                    mVideoView.addTimedTextSource(subtitlePath);
-                    mVideoView.setTimedTextShown(true);
-                    mVideoView.setTimedTextEncoding(null);
-
-                    mSubtitleView.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-
-        mVideoView.setOnTimedTextListener(new MediaPlayer.OnTimedTextListener() {
-            @Override
-            public void onTimedText(String text) {
-                mSubtitleView.setText(text);
-            }
-
-            @Override
-            public void onTimedTextUpdate(byte[] pixels, int width, int height) {
-
-            }
-        });
     }
 
     @Override
     protected void onPause() {
-        if(mVideoView != null && mVideoView.isPlaying()) {
-            mPosition = mVideoView.getCurrentPosition();
-
-            mVideoView.pause();
-        }
 
         super.onPause();
     }
@@ -101,9 +48,5 @@ public class VideoActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        if(mVideoView != null) {
-            mVideoView.stopPlayback();
-        }
     }
 }
--- app/src/main/java/com/shirlman/yishi/ui/widgets/VideoController.java
+++ app/src/main/java/com/shirlman/yishi/ui/widgets/VideoController.java
@@ -7,92 +7,14 @@ import android.view.View;
 import com.shirlman.yishi.R;
 import com.shirlman.yishi.models.VideoInfo;
 
-import io.vov.vitamio.widget.MediaController;
 
 /**
  * Created by KB-Server on 2016/6/25.
  */
-public class VideoController extends MediaController{
+public class VideoController {
     private Context mContext;
     private View mRootView;
     private boolean isLocked;
 
-    public VideoController(Context context, VideoInfo videoInfo) {
-        super(context);
 
-        mContext = context;
-    }
-
-    @Override
-    protected View makeControllerView() {
-        mRootView = LayoutInflater.from(mContext).inflate(R.layout.video_controller, this);
-
-        initControllerView();
-
-        return mRootView;
-    }
-
-    private void initControllerView() {
-        mRootView.findViewById(R.id.video_controller).setOnClickListener(onControllerClickListener);
-        mRootView.findViewById(R.id.video_lock).setOnClickListener(onLockClickListener);
-        mRootView.findViewById(R.id.video_settings).setOnClickListener(onSettingClickListener);
-        mRootView.findViewById(R.id.video_next).setOnClickListener(onNextClickListener);
-        mRootView.findViewById(R.id.mediacontroller_back).setOnClickListener(onBackClickListener);
-    }
-
-    private OnClickListener onControllerClickListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            VideoController.this.hide();
-        }
-    };
-
-    private OnClickListener onLockClickListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            View videoLockView = mRootView.findViewById(R.id.video_lock);
-            View videoControllerTop = mRootView.findViewById(R.id.video_controller_top);
-            View videoControllerBottom = mRootView.findViewById(R.id.video_controller_bottom);
-            View videoControllerRight = mRootView.findViewById(R.id.video_controller_right);
-
-            if(!isLocked) {
-                videoLockView.setScaleX(1.5f);
-                videoLockView.setScaleY(1.5f);
-
-                videoControllerTop.setVisibility(INVISIBLE);
-                videoControllerBottom.setVisibility(INVISIBLE);
-                videoControllerRight.setVisibility(INVISIBLE);
-            } else {
-                videoLockView.setScaleX(1f);
-                videoLockView.setScaleY(1f);
-
-                videoControllerTop.setVisibility(VISIBLE);
-                videoControllerBottom.setVisibility(VISIBLE);
-                videoControllerRight.setVisibility(VISIBLE);
-            }
-
-            isLocked = !isLocked;
-        }
-    };
-
-    private OnClickListener onNextClickListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-        }
-    };
-
-    private OnClickListener onSettingClickListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-        }
-    };
-
-    private OnClickListener onBackClickListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-        }
-    };
 }
